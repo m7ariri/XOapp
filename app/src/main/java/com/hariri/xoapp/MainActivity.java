bffbd5c08c1ff1a5e8150f4892bb1b2b6ebe2701
Mohammad Hariri <Moh.s.alhariri@gmail.com>
2017-11-09 01:06:19 +0300

    added a comment

--- app/src/main/java/com/hariri/xoapp/MainActivity.java
+++ app/src/main/java/com/hariri/xoapp/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         int isTapped = Integer.parseInt(XO.getTag().toString());
 
+        // The 8 winning combinations
+
         int[][] gamewinning = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7,}, {2, 5, 8},
                 {0, 4, 8,}, {2 ,4 ,6} };
 
